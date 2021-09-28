8f154cacfdaedd6a13353ca476ffb719bc0654fd
July Fernández <july.fernandezf@gmail.com>
2021-09-28 13:01:36 -0500

    Assignment

--- sketch.js
+++ sketch.js
@@ -1,13 +1,54 @@
-function preload() {
-  // put preload code here
-}
+i = 1.85 // upper mouth radian
+x = 0.2 // lower mouth radian
+speed = 0.01 // speed of mouth opening & closing
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  // put setup code here
+  createCanvas(400, 400);
+  frameRate (80) 
+
 }
 
 function draw() {
-  // put drawing code here
-  background("red");
+  background(95, 215, 236);
+
+
+rectMode (CENTER);
+line (0, 50, 400, 300);
+fill (255, 255, 255);
+
+rect (200, 150, 150, 150);
+fill (252, 108, 133);
+
+      ellipse(200, 190, 10, 10);
+
+arc(frameCount, 180, 80, 80, 100, PI + QUARTER_PI);
+arc(50, frameCount, 80, 80, 0, PI + QUARTER_PI, PIE);
+arc(50, 50, 80, 80, 0, PI + QUARTER_PI);
+arc(200, 320,80, 80, 100, PI + QUARTER_PI);
+
+fill (255, 255, 0)
+
+stroke ("green)")
+
+ellipse (200, 280, 40, 40)
+ellipse (200, 140, 40, 40)
+stroke ("yellow)")
+ellipse (200, 70, 40, 40)
+
+
+
+
+
+fill(255, 255, 0);
+  ellipse(200, 200, 50, 50);
+
+  fill(0);
+  if (i >= 2 || i <= 1.8) {
+    speed = speed*(-1) 
+  }
+  arc(200, 200, 50, 50, 
+      (i+=speed)*PI, (x-=speed)*PI, PIE);
+
+      ellipse(200, 190, 5, 5);
+      
 }
